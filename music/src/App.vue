f6659c3412237ce45015054f1397c803302f13f0
张越 <zhangyue@zhangyuedeMacBook-Pro.local>
2018-02-08 20:26:07 +0800

    init

--- music/src/App.vue
+++ music/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    Hello Vue
     <router-view/>
   </div>
 </template>
@@ -10,5 +11,11 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="less">
+@import "./common/css/variable.less";
+
+#app {
+  color: @color-theme;
+}
 </style>
+
