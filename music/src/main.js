0f070e99aba09cd3951fca4fd5a3451c4432d4c0
张越 <zhangyue@zhangyuedeMacBook-Pro.local>
2018-02-08 20:40:03 +0800

    init

--- music/src/main.js
+++ music/src/main.js
@@ -1,9 +1,13 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './common/css/index.less'
+import Fastclick from 'fastclick'
 
 Vue.config.productionTip = false
+
+Fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
