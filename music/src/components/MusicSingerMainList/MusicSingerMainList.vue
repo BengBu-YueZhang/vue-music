441653d46bb02c47403296483487b18623c75422
张越 <zhangyue@zhangyuedeMacBook-Pro.local>
2018-02-10 00:49:36 +0800

    reload

--- music/src/components/MusicSingerMainList/MusicSingerMainList.vue
+++ music/src/components/MusicSingerMainList/MusicSingerMainList.vue
@@ -2,31 +2,21 @@
     <ul class="singer-list-wrapper">
         <li v-for="(singerItem, index) in singerData"
             :key="index">
-            <music-singer-main-list-title :title="singerItem.key">
-            </music-singer-main-list-title>
+            <h3 class="singer-title">{{singerItem.key}}</h3>
             <ul class="singer-content">
-                <music-singer-main-list-content
+                <li class="singer-content-item"
                     v-for="singerContent in singerItem.items"
-                    :key="singerContent.id"
-                    :letter="singerContent.letter"
-                    :name="singerContent.name"
-                    :imgurl="singerContent.imgurl"
-                ></music-singer-main-list-content>
+                    :key="singerContent.id">
+                    <img v-lazy="singerContent.imgurl">
+                    <span>{{singerContent.name}}</span>
+                </li>
             </ul>
         </li>
     </ul>
 </template>
 
 <script>
-import MusicSingerMainListContent from './../MusicSingerMainListContent/MusicSingerMainListContent'
-import MusicSingerMainListTitle from './../MusicSingerMainListTitle/MusicSingerMainListTitle'
-
 export default {
-    components: {
-        MusicSingerMainListContent,
-        MusicSingerMainListTitle,
-    },
-
     props: {
         'singer-data': {
             type: Array,
@@ -44,5 +34,36 @@ export default {
 .singer-list-wrapper {
     width: 100%;
     height: auto;
+}
+
+.singer-title {
+    width: 100%;
+    font-size: @font-size-medium;
+    font-weight: normal;
+    color: @color-text-l;
+    line-height: 14px;
+    padding: 8px 0 8px 14px;
+    box-sizing: border-box;
+    background-color: @color-highlight-background;
+}
+
+
+.singer-content-item {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100%;
+    height: 70px;
+    padding: 20px 0 0 30px;
+    img {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+    }
+    span {
+        margin-left: 20px;
+        font-size: @font-size-medium;
+        color: @color-text-l;
+    }
 }
 </style>
