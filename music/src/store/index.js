e392acebcbdfea20f7e4e61e3346c2362d8a6304
张越 <zhangyue@zhangyuedeMacBook-Pro.local>
2018-02-10 15:08:30 +0800

    vuex

--- music/src/store/index.js
+++ music/src/store/index.js
@@ -1,13 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createLogger from 'vuex/dist/logger'
 import recommend from './modules/recommend'
 import singer from './modules/singer'
 
 Vue.use(Vuex)
 
+const debug = process.env.NODE_ENV !== 'production'
+
 export default new Vuex.Store({
     modules: {
         recommend,
         singer
-    }
+    },
+    strict: debug,
+    // 类似redux的中间件
+    plugins: debug ? [createLogger()] : []
 })
